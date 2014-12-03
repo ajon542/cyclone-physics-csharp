e4da196689f7855d7cb73e48ff0cdb40c38ab361
Andrew Jones <ajon542@gmail.com>
2014-12-03 10:10:43 -0800

    Added comments to the Ballistic demo

--- Assets/Demos/Ballistic/Ballistic.cs
+++ Assets/Demos/Ballistic/Ballistic.cs
@@ -1,13 +1,37 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Class representing a simple particle affected by gravity.
+/// </summary>
 public class Ballistic : MonoBehaviour
 {
-    private Cyclone.Particle particle = new Cyclone.Particle();
+    #region Unity Editor
 
+    /// <summary>
+    ///  The velocity of the particle.
+    /// </summary>
     public Vector3 velocity;
+
+    /// <summary>
+    /// The acceleration of the particle.
+    /// </summary>
     public Vector3 acceleration;
+
+    /// <summary>
+    /// The damping of the particle.
+    /// </summary>
     public float damping;
 
+    #endregion
+
+    /// <summary>
+    /// Create a particle instance.
+    /// </summary>
+    private Cyclone.Particle particle = new Cyclone.Particle();
+
+    /// <summary>
+    /// Set the default properties of the particle.
+    /// </summary>
     private void Start()
     {
         particle.Mass = 2.0f;
@@ -19,12 +43,19 @@ public class Ballistic : MonoBehaviour
         SetObjectPosition(particle.Position);
     }
 
+    /// <summary>
+    /// Update the particle position.
+    /// </summary>
     private void Update()
     {
-        SetObjectPosition(particle.Position);
         particle.Integrate(Time.deltaTime);
+        SetObjectPosition(particle.Position);
     }
 
+    /// <summary>
+    /// Helper method to convert a Cyclone.Math.Vector3 to a UnityEngine.Vector3 position.
+    /// </summary>
+    /// <param name="position">The position.</param>
     private void SetObjectPosition(Cyclone.Math.Vector3 position)
     {
         transform.position = new Vector3((float)position.x, (float)position.y, (float)position.z);
