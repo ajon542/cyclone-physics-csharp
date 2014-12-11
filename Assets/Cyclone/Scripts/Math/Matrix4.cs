36bfafddfee0c06b172422a26f4607c1cae4ee51
Andrew Jones <ajon542@gmail.com>
2014-12-11 10:14:42 -0800

    Implemented matrix4
    This needs to be tested heavily, ideally with a proper test framework. I
    can forsee plenty of pass by reference problems. The original C++
    implementation passes and returns many const values. C#, unfortunately,
    does not have this 'const correctness' concept.

--- Assets/Cyclone/Scripts/Math/Matrix4.cs
+++ Assets/Cyclone/Scripts/Math/Matrix4.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Cyclone.Math
 {
@@ -69,14 +66,148 @@ namespace Cyclone.Math
 
         public double GetDeterminant()
         {
-            return 0;
+            return -Data[8] * Data[5] * Data[2] +
+                    Data[4] * Data[9] * Data[2] +
+                    Data[8] * Data[1] * Data[6] -
+                    Data[0] * Data[9] * Data[6] -
+                    Data[4] * Data[1] * Data[10] +
+                    Data[0] * Data[5] * Data[10];
         }
 
-        public void SetInverse(Matrix4 matrix)
+        public void SetInverse(Matrix4 m)
         {
+            // Make sure the determinant is non-zero.
+            double det = GetDeterminant();
+            if (det == 0)
+            {
+                return;
+            }
+            det = (1.0) / det;
 
+            Data[0] = (-m.Data[9] * m.Data[6] + m.Data[5] * m.Data[10]) * det;
+            Data[4] = (m.Data[8] * m.Data[6] - m.Data[4] * m.Data[10]) * det;
+            Data[8] = (-m.Data[8] * m.Data[5] + m.Data[4] * m.Data[9]) * det;
+
+            Data[1] = (m.Data[9] * m.Data[2] - m.Data[1] * m.Data[10]) * det;
+            Data[5] = (-m.Data[8] * m.Data[2] + m.Data[0] * m.Data[10]) * det;
+            Data[9] = (m.Data[8] * m.Data[1] - m.Data[0] * m.Data[9]) * det;
+
+            Data[2] = (-m.Data[5] * m.Data[2] + m.Data[1] * m.Data[6]) * det;
+            Data[6] = (+m.Data[4] * m.Data[2] - m.Data[0] * m.Data[6]) * det;
+            Data[10] = (-m.Data[4] * m.Data[1] + m.Data[0] * m.Data[5]) * det;
+
+            Data[3] = (m.Data[9] * m.Data[6] * m.Data[3]
+                       - m.Data[5] * m.Data[10] * m.Data[3]
+                       - m.Data[9] * m.Data[2] * m.Data[7]
+                       + m.Data[1] * m.Data[10] * m.Data[7]
+                       + m.Data[5] * m.Data[2] * m.Data[11]
+                       - m.Data[1] * m.Data[6] * m.Data[11]) * det;
+            Data[7] = (-m.Data[8] * m.Data[6] * m.Data[3]
+                       + m.Data[4] * m.Data[10] * m.Data[3]
+                       + m.Data[8] * m.Data[2] * m.Data[7]
+                       - m.Data[0] * m.Data[10] * m.Data[7]
+                       - m.Data[4] * m.Data[2] * m.Data[11]
+                       + m.Data[0] * m.Data[6] * m.Data[11]) * det;
+            Data[11] = (m.Data[8] * m.Data[5] * m.Data[3]
+                       - m.Data[4] * m.Data[9] * m.Data[3]
+                       - m.Data[8] * m.Data[1] * m.Data[7]
+                       + m.Data[0] * m.Data[9] * m.Data[7]
+                       + m.Data[4] * m.Data[1] * m.Data[11]
+                       - m.Data[0] * m.Data[5] * m.Data[11]) * det;
         }
 
+        public Matrix4 Inverse()
+        {
+            Matrix4 result = new Matrix4();
+            result.SetInverse(this);
+            return result;
+        }
 
+        public void Invert()
+        {
+            SetInverse(this);
+        }
+
+        public Vector3 TransformDirection(Vector3 vector)
+        {
+            return new Vector3
+               (
+                vector.x * Data[0] +
+                vector.y * Data[1] +
+                vector.z * Data[2],
+
+                vector.x * Data[4] +
+                vector.y * Data[5] +
+                vector.z * Data[6],
+
+                vector.x * Data[8] +
+                vector.y * Data[9] +
+                vector.z * Data[10]
+                );
+        }
+
+        public Vector3 TransformInverseDirection(Vector3 vector)
+        {
+            return new Vector3
+                (
+                vector.x * Data[0] +
+                vector.y * Data[4] +
+                vector.z * Data[8],
+
+                vector.x * Data[1] +
+                vector.y * Data[5] +
+                vector.z * Data[9],
+
+                vector.x * Data[2] +
+                vector.y * Data[6] +
+                vector.z * Data[10]
+                );
+        }
+
+        public Vector3 TransformInverse(Vector3 vector)
+        {
+            Vector3 tmp = vector;
+            tmp.x -= Data[3];
+            tmp.y -= Data[7];
+            tmp.z -= Data[11];
+            return new Vector3
+                (
+                tmp.x * Data[0] +
+                tmp.y * Data[4] +
+                tmp.z * Data[8],
+
+                tmp.x * Data[1] +
+                tmp.y * Data[5] +
+                tmp.z * Data[9],
+
+                tmp.x * Data[2] +
+                tmp.y * Data[6] +
+                tmp.z * Data[10]
+                );
+        }
+
+        public Vector3 GetAxisVector(int i)
+        {
+            return new Vector3(Data[i], Data[i + 4], Data[i + 8]);
+        }
+
+        public void SetOrientationAndPos(Quaternion q, Vector3 pos)
+        {
+            //Data[0] = 1 - (2 * q.j * q.j + 2 * q.k * q.k);
+            //Data[1] = 2 * q.i * q.j + 2 * q.k * q.r;
+            //Data[2] = 2 * q.i * q.k - 2 * q.j * q.r;
+            //Data[3] = pos.x;
+
+            //Data[4] = 2 * q.i * q.j - 2 * q.k * q.r;
+            //Data[5] = 1 - (2 * q.i * q.i + 2 * q.k * q.k);
+            //Data[6] = 2 * q.j * q.k + 2 * q.i * q.r;
+            //Data[7] = pos.y;
+
+            //Data[8] = 2 * q.i * q.k + 2 * q.j * q.r;
+            //Data[9] = 2 * q.j * q.k - 2 * q.i * q.r;
+            //Data[10] = 1 - (2 * q.i * q.i + 2 * q.j * q.j);
+            //Data[11] = pos.z;
+            throw new NotImplementedException();
+        }
     }
 }
