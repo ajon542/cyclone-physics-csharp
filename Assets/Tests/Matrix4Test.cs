3bca3c4939aff74afc457e9edab45d3bbb85cb2f
Andrew Jones <ajon542@gmail.com>
2014-12-12 08:48:09 -0800

    Matrix determinant bug fixed.
    SetInverse is meant to set the current matrix to the inverse of the
    matrix that is passed as an argument. The problem was we were
    calculating the determinant of the current matrix rather than the matrix
    that was being passed as an argument.

--- Assets/Tests/Matrix4Test.cs
+++ Assets/Tests/Matrix4Test.cs
@@ -15,6 +15,8 @@ public class Matrix4Test : MonoBehaviour
         Debug.Log("Matrix4Test.Test3 " + (Test3() ? "Succeeded" : "Failed"));
         Debug.Log("Matrix4Test.Test4 " + (Test4() ? "Succeeded" : "Failed"));
         Debug.Log("Matrix4Test.Test5 " + (Test5() ? "Succeeded" : "Failed"));
+        Debug.Log("Matrix4Test.Test6 " + (Test6() ? "Succeeded" : "Failed"));
+        Debug.Log("Matrix4Test.Test7 " + (Test7() ? "Succeeded" : "Failed"));
     }
 
     /// <summary>
@@ -54,13 +56,13 @@ public class Matrix4Test : MonoBehaviour
     /// <returns><c>true</c> if test succeeded; otherwise, <c>false</c>.</returns>
     private bool Test2()
     {
-        var m1 = new Matrix4
+        Matrix4 m1 = new Matrix4
             (
             1.0, 2.0, 3.0, 4.0,
             1.0, 2.0, 3.0, 4.0,
             1.0, 2.0, 3.0, 4.0
             );
-        var m2 = new Matrix4
+        Matrix4 m2 = new Matrix4
             (
             1.0, 0.0, 0.0, 0.0,
             0.0, 1.0, 0.0, 0.0,
@@ -85,7 +87,7 @@ public class Matrix4Test : MonoBehaviour
     /// <returns><c>true</c> if test succeeded; otherwise, <c>false</c>.</returns>
     private bool Test3()
     {
-        var m1 = new Matrix4
+        Matrix4 m1 = new Matrix4
             (
             1.0, 0.0, 0.0, 1.0,
             0.0, 2.0, 1.0, 2.0,
@@ -103,7 +105,7 @@ public class Matrix4Test : MonoBehaviour
     /// <returns><c>true</c> if test succeeded; otherwise, <c>false</c>.</returns>
     private bool Test4()
     {
-        var m1 = new Matrix4
+        Matrix4 m1 = new Matrix4
             (
             1.0, 2.0, 3.0, 4.0,
             5.0, 6.0, 7.0, 8.0,
@@ -117,27 +119,54 @@ public class Matrix4Test : MonoBehaviour
 
     private bool Test5()
     {
-        var tmp = new Matrix4();
-
-        var m1 = new Matrix4
+        Matrix4 m1 = new Matrix4
             (
             1.0, 0.0, 0.0, 1.0,
             0.0, 2.0, 1.0, 2.0,
             2.0, 1.0, 0.0, 1.0
             );
 
-        var expected = new Matrix4
+        double det = m1.GetDeterminant();
+
+        return det == -1.0;
+    }
+
+    private bool Test6()
+    {
+        Matrix4 m1 = new Matrix4
             (
-            -1.0, 0.0, 0.0, 1.0,
-            2.0, 0.0, -1.0, -1.0,
-            -4.0, -1.0, 2.0, 4.0
+            1.0, 0.0, 0.0, 1.0,
+            0.0, 2.0, 1.0, 2.0,
+            2.0, 1.0, 0.0, 1.0
             );
 
-        tmp.SetInverse2(m1);
+        Matrix4 inverse = new Matrix4();
+        inverse.SetInverse(m1);
 
-        Matrix4 inverse = m1 * expected;
-        Matrix4 result = new Matrix4();
+        Matrix4 expected = new Matrix4
+            (
+            1.0, 0.0, 0.0, -1.0,
+            -2.0, 0.0, 1.0, 1.0,
+            4.0, 1.0, -2.0, -4.0
+            );
 
-        return result == (m1 * tmp);
+        return inverse == expected;
+    }
+
+    private bool Test7()
+    {
+        Matrix4 m1 = new Matrix4
+            (
+            1.0, 0.0, 0.0, 1.0,
+            0.0, 2.0, 1.0, 2.0,
+            2.0, 1.0, 0.0, 1.0
+            );
+
+        Matrix4 inverse = new Matrix4();
+        inverse.SetInverse(m1);
+
+        Matrix4 identity = new Matrix4();
+
+        return identity == (m1 * inverse);
     }
 }
