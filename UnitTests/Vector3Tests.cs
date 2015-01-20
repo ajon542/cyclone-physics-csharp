ff22c1e24ea66646a9d09b9e5194c808aa55fdb6
Andrew Jones <ajon542@gmail.com>
2015-01-20 12:56:16 -0800

    Added cross product tests.

--- UnitTests/Vector3Tests.cs
+++ UnitTests/Vector3Tests.cs
@@ -63,11 +63,44 @@ namespace UnitTests
             // The vector class has two methods for calculating dot product.
             // Make sure they produce the same results.
             Vector3 v1 = new Vector3(1, 2, 3);
-            Vector3 v2 = new Vector3(1, 2, 3);
+            Vector3 v2 = new Vector3(11, -24, -7);
 
             Assert.AreEqual(v1 * v2, v1.ScalarProduct(v2));
             Assert.AreEqual(v1 * v2, v2.ScalarProduct(v1));
             Assert.AreEqual(v1 * v2, v2 * v1);
+        }
+
+        /// <summary>
+        /// Tests the dot product of orthogonal vectors are zero.
+        /// </summary>
+        [TestMethod]
+        public void TestDotProduct2()
+        {
+            Vector3 v1 = new Vector3(1, 0, 0);
+            Vector3 v2 = new Vector3(0, 1, 0);
+            Vector3 v3 = new Vector3(0, 0, 1);
+
+            Assert.AreEqual(v1 * v2, 0);
+            Assert.AreEqual(v1 * v3, 0);
+            Assert.AreEqual(v2 * v3, 0);
+        }
+
+        /// <summary>
+        /// Tests the cross product.
+        /// </summary>
+        [TestMethod]
+        public void TestCrossProduct1()
+        {
+            // Left handed coordinate system.
+            Vector3 v1 = new Vector3(1, 0, 0);
+            Vector3 v2 = new Vector3(0, -1, 0);
+            Vector3 v3 = new Vector3(0, 0, 1);
+
+            Assert.AreEqual(v1.VectorProduct(v3), v2);
+
+            // Right handed coordinate system.
+            v2.Invert();
+            Assert.AreEqual(v3.VectorProduct(v1), v2);
         }
     }
 }
